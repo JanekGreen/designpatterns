338582d6931a39ab20a7787921d0025d58a33099
Pawel Wojcik <pawel.wojcik@intive.com>
2022-08-05 22:24:25 +0200

    Iterator

---
+++ src/main/java/iterator/People.java
@@ -0,0 +1,16 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class People implements Iterable<String> {
+    List<String> people = Arrays.asList("Janek", "Czesława", "Roman", "Jadzia");
+
+    @Override
+    public Iterator<String> iterator() {
+        return people.iterator();
+    }
+
+}
