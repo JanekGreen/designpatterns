5a3fd23b8987f6b8924a0247a124e18c1511a817
Pawel Wojcik <pawel.wojcik@intive.com>
2022-10-17 22:29:04 +0200

    cleanup

--- src/main/java/strategy/DiscountStrategy.java
+++ src/main/java/strategy/DiscountStrategy.java
@@ -5,19 +5,12 @@ import java.util.function.Function;
 
 public class DiscountStrategy {
     private Discount discount;
-   private Function<BigDecimal, BigDecimal> discountFunc;
 
-/*    public DiscountStrategy(Discount discount) {
+    public DiscountStrategy(Discount discount) {
         this.discount = discount;
-    }*/
-    public DiscountStrategy(Function<BigDecimal, BigDecimal> discountFunc) {
-       this.discountFunc = discountFunc;
     }
     public BigDecimal getPrice(BigDecimal price){
        return discount.calculateDiscount(price);
-    }
-    public BigDecimal getPriceFunc(BigDecimal price){
-        return discountFunc.apply(price);
     }
 
 }
--- src/main/java/strategy/Main.java
+++ src/main/java/strategy/Main.java
@@ -8,22 +8,16 @@ import java.util.function.Function;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, Function<BigDecimal,BigDecimal>> strategyMap = new HashMap<>();
-        strategyMap.put("christmas", originalPrice -> originalPrice.multiply(BigDecimal.valueOf(0.80d)));
 
-
-/*      DiscountStrategy strategy = new DiscountStrategy(new ChristmasDiscount());
+        DiscountStrategy strategy = new DiscountStrategy(new ChristmasDiscount());
         BigDecimal price = strategy.getPrice(BigDecimal.valueOf(100d));
         System.out.println(price);
         strategy = new DiscountStrategy(new EasterDiscount());
         price = strategy.getPrice(BigDecimal.valueOf(100d));
         System.out.println(price);
-        BigDecimal originalPrice = BigDecimal.valueOf(100d);*/
-        DiscountStrategy functionStrategy = new DiscountStrategy(originalPrice -> originalPrice.multiply(BigDecimal.valueOf(0.80d)));
-        BigDecimal priceFunc = functionStrategy.getPriceFunc(BigDecimal.valueOf(100d));
-        System.out.println("func "+priceFunc);
-        BigDecimal christmas = strategyMap.get("christmas").apply(BigDecimal.valueOf(100d));
-        System.out.println(christmas);
+        BigDecimal originalPrice = BigDecimal.valueOf(100d);
+        System.out.println(originalPrice);
+
 
     }
 
