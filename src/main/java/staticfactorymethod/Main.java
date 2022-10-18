b4178d67eeb313295e2b1b74005010f11f88d91e
Pawel Wojcik <pawel.wojcik@intive.com>
2022-10-18 21:13:35 +0200

    added main

---
+++ src/main/java/staticfactorymethod/Main.java
@@ -0,0 +1,12 @@
+package staticfactorymethod;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Collections.emptyList();
+        List<Integer> integers = List.of(1, 2, 3);
+
+    }
+}
