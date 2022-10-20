d0259f069721f3412186a664e40f71aa181f077a
Pawel Wojcik <pawel.wojcik@intive.com>
2022-10-20 19:28:43 +0200

    Mutable/vs non-mutable list

--- src/main/java/builder/Main.java
+++ src/main/java/builder/Main.java
@@ -2,6 +2,7 @@ package builder;
 
 import staticfactorymethod.ShapeData;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,7 +22,14 @@ public class Main {
         System.out.println(user);
 
         /* static factory method*/
+        /*Lista jest niemutowalna UnsupportedOperationException zostanie wyrzucony przy jakiejkolwiek modyfikacji*/
         List<String> names = Arrays.asList("cos", "coś2", "coś3");
+        /*jak temu zaradzić: użyć Konstruktora ArrayList */
+        List<String> namesMutable = new ArrayList<>(Arrays.asList("cos", "coś2", "coś3"));
+
+        /*to się wykrzaczy*/
+        //names.add("Heniu");
+        namesMutable.add("Józio");
 
         Dog dog = new Dog.DogBuilder()
                 .toys(Arrays.asList("cos", "coś2", "coś3"))
