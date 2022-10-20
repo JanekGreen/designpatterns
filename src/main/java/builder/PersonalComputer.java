d3f28b07fc38ea7b715c2fa6de3cd70e1a131693
Pawel Wojcik <pawel.wojcik@intive.com>
2022-10-20 20:53:14 +0200

    Zadanie Builder

---
+++ src/main/java/builder/PersonalComputer.java
@@ -0,0 +1,12 @@
+package builder;
+
+public class PersonalComputer {
+    private String cpu;
+    private int numberOfCores;
+    private String graphicsCard;
+    private String ram;
+    private String motherboard;
+    private String soundCard;
+    private boolean floppyDisk;
+
+}
